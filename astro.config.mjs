48e91793b990b6d282132013527fd0bf90b399c7
HWT-0602 <s88allen602@gmail.com>
2024-10-18 19:29:30 +0800

    準備部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site:'https://HWT-0602.github.io',
+  base:'brightenways'
 });
